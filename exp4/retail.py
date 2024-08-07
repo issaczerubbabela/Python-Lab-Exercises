9b8198630b76c7c0439924ab849713e1dde6cbf3
issaczerubbabela <126003111@sastra.ac.in>
2024-08-07 17:06:03 +0530

    question 3 added

---
+++ exp4/retail.py
@@ -0,0 +1,6 @@
+'''Develop a system to manage inventory for a retail store. Discuss how you would organize
+product information, track stock levels, handle orders and returns, and generate reports on
+sales and inventory turnover.'''
+
+def addItem(Inventory):
+    Inventory['name'] = input('enter product name')
